fc96d031be948d576176e85d28f475dc5ad83d18
studentb <studentb@aitc.eif.local>
2018-10-18 20:55:28 +0400

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest {
+
+    WebDriver driver;
+    @Test
+
+    public void successLogin()
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Lusine\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+        assertTrue(driver.getCurrentUrl().contains("http://google.comm"), "The page has incorrect URL");
+
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
