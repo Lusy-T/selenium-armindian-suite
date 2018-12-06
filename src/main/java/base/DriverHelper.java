962c41a8f9d1ee5feed37eaf716ecb41d5da0ebe
studentb <lusy_tigr@mail.ru>
2018-12-06 18:29:41 +0400

    06.12. At the beginig of the lesson, no error
    ?? https://app.saucelabs.com/login is not work??

--- src/main/java/base/DriverHelper.java
+++ src/main/java/base/DriverHelper.java
@@ -3,6 +3,49 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverHelper {
+    public static DriverHelper get() {
+        DriverHelper driverHelper = new DriverHelper();
+        return driverHelper;
+    }
+    public WebDriver driver;
+    private static final String BROWSER = System.getProperty("selenium.browser", "chrome");
+    private static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    public WebDriver getDriver() {
+        if (driverThread.get() == null) {
+            switch (BROWSER) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver",
+                            "./src/main/resources/drivers/chromedriver-mac-64bit");
+                    driver = new ChromeDriver();
+                    driverThread.set(driver);
+                    break;
+
+                case "firefox":
+                    System.setProperty("webdriver.gecko.driver",
+                            "./src/main/resources/drivers/geckodriver-mac-64bit");
+                    driver = new FirefoxDriver();
+                    driverThread.set(driver);
+                    break;
+            }
+        }
+        return driverThread.get();
+    }
+
+    public void quitDriver(WebDriver driver) {
+        driver.quit();
+        driverThread.remove();
+    }
+}
+
+/*
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -58,3 +101,4 @@ public class DriverHelper {
         driverThread.remove();
     }
 }
+*/
